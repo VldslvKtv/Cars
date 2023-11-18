174315b1980bd71f25b80fd8bdf580e2b747c607
Vladislav <vladkitov2003@mail.ru>
2023-11-18 23:15:53 +0300

    Созданы модели и парсер xml файла(дополненно).

--- cars/parse_auto/views.py
+++ cars/parse_auto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import xml.etree.ElementTree as ET
+import sqlite3 as sl
 
 TREE = ET.parse("static/parse_auto/cars.xml")
 ROOT = TREE.getroot()
@@ -13,17 +14,23 @@ def get_one_elem(element):
     return new_el[:n]
 
 
-def marks_and_models(tree, root):
-    for child in root:
-        marka = child.attrib['name']
+def get_marks_and_models(tree, root):
+    con = sl.connect('db.sqlite3')
+    cursor = con.cursor()
+    for mrk in root:
+        marka = mrk.attrib['name']
+        cursor.execute('INSERT INTO parse_auto_marks (mark) values(?)', marka)
         print(f"Марка: {marka}")
-        s = tree.findall(f"mark[@name='{marka}']/")
+        mdl = tree.findall(f"mark[@name='{marka}']/")
         print(f"Модели:")
+        mark_id = cursor.execute('SELECT id FROM parse_auto_marks WHERE mark=?', marka)
         models = []
-        for elem in s:
+        for elem in mdl:
             if elem.tag == 'folder':
+                cursor.execute('INSERT INTO parse_auto_models (model, fk_model_id) values(?, ?)',
+                               (get_one_elem(elem.attrib['name']), int(mark_id),))
                 models.append(get_one_elem(elem.attrib['name']))
         print(models)
 
 
-marks_and_models(TREE, ROOT)
+get_marks_and_models(TREE, ROOT)
